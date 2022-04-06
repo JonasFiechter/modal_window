31e3f0afd0036ae157a4b56fe32b96dab61e60cd
JonasFiechter <jonasrfiechter@gmail.com>
2022-04-06 10:42:39 -0300

    update
    now it is done

--- script.js
+++ script.js
@@ -5,8 +5,22 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
 for (let i = 0; i < btnsOpenModal.length; i++)
-    btnsOpenModal[i].addEventListener('click', () => {
-        console.log(`${btnsOpenModal[i].textContent}`)
-        modal.classList.remove('hidden')
-    });
+    btnsOpenModal[i].addEventListener('click', openModal);
+    btnCloseModal.addEventListener('click', closeModal);
+    overlay.addEventListener('click', closeModal);
+
+
+document.addEventListener('keydown', (e) => {
+    if (e.key == 'Escape') {closeModal()}
+})
